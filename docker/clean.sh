615a4038620c8ee9d74225509d79bfe2ebb55075
Alejandro de Frutos <adefrutoscasado@gmail.com>
2023-01-19 18:31:11 +0100

    Remove repeated line from clean script

--- docker/clean.sh
+++ docker/clean.sh
@@ -6,10 +6,8 @@
 rm -rf ../backend/node_modules/
 rm ../backend/sync-package-lock/package-lock.json
 rm ../backend/package-lock.json
-rm -rf ../backend/node_modules/
 
 # Frontend
 rm -rf ../frontend/node_modules/
 rm ../frontend/sync-package-lock/package-lock.json
 rm ../frontend/package-lock.json
-rm -rf ../frontend/node_modules/
